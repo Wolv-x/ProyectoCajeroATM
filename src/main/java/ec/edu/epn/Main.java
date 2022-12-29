07c72433c299305a86af5eee3c67233a6703c78a
Dennis Lincango <dennis.lincango@epn.edu.ec>
2022-12-28 20:24:33 -0500

    Corrección de errores

--- src/main/java/ec/edu/epn/Main.java
+++ src/main/java/ec/edu/epn/Main.java
@@ -3,5 +3,6 @@ package ec.edu.epn;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Nueva rama");
     }
 }
