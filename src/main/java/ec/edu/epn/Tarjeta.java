0085352fe7c06a01789aa557f38b6ffbde4a6e67
Dennis Lincango <dennis.lincango@epn.edu.ec>
2023-01-05 22:16:28 -0500

    Adición de funciones para cambiar clave

--- src/main/java/ec/edu/epn/Tarjeta.java
+++ src/main/java/ec/edu/epn/Tarjeta.java
@@ -30,32 +30,40 @@ public class Tarjeta {
         System.out.println("Ingrese su nueva clave");
         String claveNueva = ingreso.getEntrada();
 
-        if (claveNueva.length() == 3 && Teclado.isNumeric(claveNueva)) {
-            if (!(clave.equals(claveNueva))) {
+        System.out.println("Ingrese nuevamente su nueva clave");
+        String claveNuevaNueva = ingreso.getEntrada();
 
-                System.out.println("#################");
-                for (int i = 0; i < baseDeDatos.cuentas.size(); i++) {
-                    if (baseDeDatos.cuentas.get(i).tarjeta.equals(numeroTarjeta) && baseDeDatos.cuentas.get(i).clave.equals(clave)) {
-                        System.out.println("CUENTA:----");
-                        System.out.println(baseDeDatos.cuentas.get(i));
-                        System.out.println("TARJETA-----");
-                        System.out.printf(baseDeDatos.cuentas.get(i).tarjeta);
-                        System.out.println("\nCLAVE-----");
-                        System.out.println(baseDeDatos.cuentas.get(i).clave);
-                        baseDeDatos.cuentas.get(i).setClave(claveNueva);
-                        System.out.println("\nClave cambiada.");
-                        System.out.println("CLAVE NUEVA-----");
-                        System.out.println(baseDeDatos.cuentas.get(i).getClave());
-                        System.out.println("Sus datos son:");
-                        System.out.println(baseDeDatos.cuentas.get(i));
+        if (claveNueva.equals(claveNuevaNueva)) {
+            if (claveNueva.length() == 3 && Teclado.isNumeric(claveNueva)) {
+                if (!(clave.equals(claveNueva))) {
+
+                    System.out.println("#################");
+                    for (int i = 0; i < baseDeDatos.cuentas.size(); i++) {
+                        if (baseDeDatos.cuentas.get(i).tarjeta.equals(numeroTarjeta) && baseDeDatos.cuentas.get(i).clave.equals(clave)) {
+                            System.out.println("CUENTA:----");
+                            System.out.println(baseDeDatos.cuentas.get(i));
+                            System.out.println("TARJETA-----");
+                            System.out.printf(baseDeDatos.cuentas.get(i).tarjeta);
+                            System.out.println("\nCLAVE-----");
+                            System.out.println(baseDeDatos.cuentas.get(i).clave);
+                            baseDeDatos.cuentas.get(i).setClave(claveNueva);
+                            System.out.println("\nHa cambiado satisfactoriamente su clave"); //NO BORRAR
+                            System.out.println("CLAVE NUEVA-----");
+                            System.out.println(baseDeDatos.cuentas.get(i).getClave());
+                            System.out.println("Sus datos son:");
+                            System.out.println(baseDeDatos.cuentas.get(i));
+                        }
                     }
+                    System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+                } else {
+                    System.out.println("La clave ingresada es la misma.");
                 }
-                System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
             } else {
-                System.out.println("La clave ingresada es la misma.");
+                System.out.println("No se ha cambiado la clave");
             }
         } else {
-            System.out.println("No se ha cambiado la clave");
+            System.out.println("Las contraseñas no coinciden. Saliendo del sistema...."); //Para más elegancia se puso ese print, pero se puede eliminar
+            System.exit(0);
         }
 
     }
