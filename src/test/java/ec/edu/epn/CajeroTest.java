7922c45dec99050652b91c99ac947abac5a14d4d
nicoToscano <nicolas.toscano@epn.edu.ec>
2023-01-08 16:20:22 -0500

    Clase Cajero Test y Tarjeta Test

--- src/test/java/ec/edu/epn/CajeroTest.java
+++ src/test/java/ec/edu/epn/CajeroTest.java
@@ -1,4 +1,18 @@
 package ec.edu.epn;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 public class CajeroTest {
+
+    Cajero cajero;
+
+    @Before
+    public void setUp() {
+        cajero = new Cajero();
+    }
+    @Test
+    public void given__when__then() {
+
+    }
 }
